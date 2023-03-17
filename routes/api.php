b19cda7680ae124bea230b63caafb8e0aa792572
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 11:37:54 +0100

    create route api for category

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('category',CategoryController::class);
+
+
 Route::group([
 
     'middleware' => 'api',
