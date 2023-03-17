37c1b6a815af406b7719549b5a00eb1585940076
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 11:42:01 +0100

    add route me

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,6 @@ Route::group([
     Route::post('login', [AuthController::class,'login']);
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
-    
+    Route::post('me', [AuthController::class,'me']);
   });
   
