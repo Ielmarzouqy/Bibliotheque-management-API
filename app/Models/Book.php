7368ea5a9191923fa97fd94e98839b0a01e56677
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 17:00:36 +0100

    add relationships between tables

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,12 +11,15 @@ class Book extends Model
     protected $fillable = [
         'title','author','isbn','location','pages','content','category_id','user_id','collection_id','status_id'
     ];
+
     public function category(){
         return $this->belongsToMany(Category::class,'categories_articles', 'book_id','category_id');
     }
+
     public function collection(){
         return $this->belongsTo(Collection::class);
     }
+    
     public function status(){
         return $this->belongsTo(Status::class);
     }
