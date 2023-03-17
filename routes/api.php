125aff4192b7083d458a46703975644c73074505
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 11:39:32 +0100

    add route logout

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::group([
   
     Route::post('register', [AuthController::class,'register']);
     Route::post('login', [AuthController::class,'login']);
+    Route::post('logout', [AuthController::class,'logout']);
    
   });
   
