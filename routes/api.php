d633c6197398caba64d0f772083e77e8484f0d8c
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 11:41:11 +0100

    add route refresh

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::group([
     Route::post('register', [AuthController::class,'register']);
     Route::post('login', [AuthController::class,'login']);
     Route::post('logout', [AuthController::class,'logout']);
-   
+    Route::post('refresh', [AuthController::class,'refresh']);
+    
   });
   
