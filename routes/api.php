7aa8756e6a240dfc6e62c55408a8d7eadb3c44c1
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 11:35:34 +0100

    create route api

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+  
+  ], function ($router) {
+  
+    Route::post('register', [AuthController::class,'register']);
+   
+  });
+  
