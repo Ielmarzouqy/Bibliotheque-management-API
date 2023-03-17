f1373dfc9c79f5b495027be2b702745ec3878541
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-03-17 10:19:00 +0100

    install jwt

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,18 +7,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    
-public function index(){
-    $category = Category::all();
-    return  response()->json(['request'=>'success','all categories'=>$category],200);
-}
 
-public function store (){
-
-}
-public function update(){
-
-}
 public function destroy(){
 
 }
--- routes/api.php
+++ routes/api.php
@@ -20,19 +20,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('category',CategoryController::class);
-
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-  
-  ], function ($router) {
-  
-    Route::post('register', [AuthController::class,'register']);
-    Route::post('login', [AuthController::class,'login']);
-    Route::post('logout', [AuthController::class,'logout']);
-    Route::post('refresh', [AuthController::class,'refresh']);
-    Route::post('me', [AuthController::class,'me']);
-  });
-  
